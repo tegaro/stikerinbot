9ffaaf6d1e08e2331774072dcca7d660e43354bd
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-05 22:13:47 +0700

    rename stikerin & ariffb delete github & tgl islam

--- plugins/menu.js
+++ plugins/menu.js
@@ -15,13 +15,10 @@ const defaultMenu = {
 ├ %totalexp XP secara Total
 │ 
 ├ Tanggal: *%week %weton, %date*
-├ Tanggal Islam: *%dateIslamic*
 ├ Waktu: *%time*
 │
 ├ Uptime: *%uptime (%muptime)*
 ├ Database: %rtotalreg dari %totalreg
-├ Github:
-├ %github
 └────
 %readmore`.trimStart(),
   header: '┌─〔 %category 〕',
@@ -377,7 +374,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'made with ❤️ by ariffb', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'made with ❤️ by ucup', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
