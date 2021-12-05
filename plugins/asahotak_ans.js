ee9b68a66e61fd63dc8fc7eaf25a47683d385453
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-05 14:12:42 +0700

    full ganti dari sc sblmnya

--- plugins/asahotak_ans.js
+++ plugins/asahotak_ans.js
@@ -1,8 +1,6 @@
 const similarity = require('similarity')
 const threshold = 0.72
-
 let handler = m => m
-
 handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return !0
@@ -13,7 +11,7 @@ handler.before = async function (m) {
         if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, '© stikerin', 'Asah Otak', '.asahotak', m)
+            await this.sendButton(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, '© ollie', 'Asah Otak', '.asahotak', m)
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
@@ -21,5 +19,6 @@ handler.before = async function (m) {
     }
     return !0
 }
+handler.exp = 0
 
 module.exports = handler
