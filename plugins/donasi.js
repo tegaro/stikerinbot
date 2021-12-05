9b2aa58fd547d8dc8390f6f19a6f36a6170d6af0
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-05 19:03:04 +0700

    ganti donasi & rename stikerin

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ GOPAY / OVO
+├ 082264247854
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© Ollie', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
