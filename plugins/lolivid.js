7686bc606e5bae16f42325fa9129b9712e9a4cd3
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-06 23:50:49 +0700

    Update lolivid.js

--- plugins/lolivid.js
+++ plugins/lolivid.js
@@ -1,34 +1,8 @@
-let fetch = require('node-fetch')
-     let handler  = async (m, { conn, args }) => {
-     let apinya = global.API('caliph', '/asupan/loli', null, 'apikey');
-let buttons = [
-  {buttonId: '/lolivid', buttonText: {displayText: 'Get Again'}, type: 1}
-]
-const buttonsMessage = {
-    contentText: `
-Asupan Lolinya kaak...
-`.trim(),    footerText: 'Ollie by @ucup',
-    buttons: buttons,
-  videoMessage: await conn.toMSG({ url: apinya }, 'videoMessage'),
-    headerType: 5
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, API('caliph', '/asupan/loli', {}, 'apikey'), 'asupan.mp4', '© Ollie')
 }
-const sendMsg = await conn.prepareMessageFromContent(m.chat,{buttonsMessage},{ quoted: m})
-
-conn.relayWAMessage(sendMsg)
-}
-handler.help = ['lolivid', 'asupanloli']
+handler.help = ['asupan']
 handler.tags = ['fun']
-handler.command = /^(lolivid|asupanloli)$/i
-handler.owner = false
-handler.mods = false
-handler.limit = true 
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.command = /^(asupan)$/i
 
 module.exports = handler
