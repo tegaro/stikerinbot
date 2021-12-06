74504f6ca08762d37b5fc9756c6d42d503355399
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-06 21:56:56 +0700

    Update nhentai.js

--- plugins/nhentai.js
+++ plugins/nhentai.js
@@ -26,7 +26,7 @@ let handler = async (m, { usedPrefix, conn, args, text}) => {
 handler.help = ['nhsearch', 'nhentaisearch'].map(v => v + ' <query>')
 handler.tags = ['internet']
 handler.command = /^(nh|nhentai)(search)$/i
-handler.limit = 1
+handler.limit = true
 module.exports = handler
 
 async function getBuffer(url) {
