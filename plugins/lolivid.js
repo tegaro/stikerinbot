487e4ddc9635ebd0c839d4b707ff2171c9c388f1
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-06 23:55:45 +0700

    Delete lolivid.js

--- plugins/lolivid.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('caliph', '/asupan/loli', {}, 'apikey'), 'asupan.mp4', '© Ollie')
-}
-handler.help = ['lolivid', 'asupanloli']
-handler.tags = ['fun']
-handler.command = /^(lolivid|asupanloli)$/i
-
-module.exports = handler
