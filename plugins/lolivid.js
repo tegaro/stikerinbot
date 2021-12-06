246153ee7617a8a30b784b27d0c1793100410d71
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-06 23:51:49 +0700

    Update lolivid.js

--- plugins/lolivid.js
+++ plugins/lolivid.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, API('caliph', '/asupan/loli', {}, 'apikey'), 'asupan.mp4', '© Ollie')
 }
-handler.help = ['asupan']
+handler.help = ['lolivid', 'asupanloli']
 handler.tags = ['fun']
-handler.command = /^(asupan)$/i
+handler.command = /^(lolivid|asupanloli)$/i
 
 module.exports = handler
