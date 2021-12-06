8000809a6fdfbbf06c18c0b1c6aa9c709029542d
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-06 22:03:02 +0700

    Update nsfwloli.js

--- plugins/nsfwloli.js
+++ plugins/nsfwloli.js
@@ -1,10 +1,11 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, args }) => {
-    heum = await require('node-fetch')('https://raw.githubusercontent.com/Caliph71/txt/main/nsfwloli.json').then(v => v.json())
+    heum = await require('node-fetch')('https://raw.githubusercontent.com/Caliph71/txt/main/nsfwloli.json').then(v => v.json())
+
    conn.sendFile(m.chat, heum[Math.floor(Math.random() * heum.length)], 'Server Error!', 'Lolinya Kaak', m, false)
 }
 handler.help = ['nsfwloli']
-handler.tags = ['sange']
+handler.tags = ['internet']
 handler.command = /^nsfwloli$/i
 handler.owner = false
 handler.mods = false
