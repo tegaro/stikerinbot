6d3766a1e3daccc502999d3e6baf44b55fb58737
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-07 00:32:13 +0700

    Update wallpaperAnime.js

--- plugins/wallpaperAnime.js
+++ plugins/wallpaperAnime.js
@@ -1,15 +1,14 @@
+const { MessageType } = require('@adiwajshing/baileys')
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-    let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
-    if (!res.ok) throw eror
-    let img = await res.buffer()
-    if (!img) throw img
-    conn.sendFile(m.chat, img, '', '© stikerin', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+await m.reply('Sedang Di Proses...')
+   conn.sendFile(m.chat, 'https://recoders-area.caliph.repl.co/api/wpnime', 'wallpaper.jpeg', 
+'Wallpaper Nya Kak', m)
 }
-handler.help = ['wallpaperanime']
+handler.help = ['wallpaper', 'wp']
 handler.tags = ['internet']
-handler.command = /^(wallpaper|wp)anime$/i
+handler.command = /^(wallpaper|wp)$/i
 handler.limit = true
 
 module.exports = handler
