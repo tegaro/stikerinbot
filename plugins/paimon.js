fffe0df12022a505a313f5adbfcd30325f9328f5
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-07 23:32:55 +0700

    Create paimon.js

---
+++ plugins/paimon.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/baal.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'emergency food!!', '© ollie', 'Get Again', '/paimon', m)
+}
+handler.command = /^(paimon)$/i
+handler.tags = ['anime']
+handler.help = ['paimon']
+module.exports = handler
