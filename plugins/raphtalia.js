3ac025b3c2c3fd62ad487410f8aa727029b96831
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-07 19:22:58 +0700

    Update raphtalia.js

--- plugins/raphtalia.js
+++ plugins/raphtalia.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/hololive.json')
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/raphtalia.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Hololive', '© ollie', 'Get Again', '/hololive', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Raphtalia Mpshh', '© ollie', 'Get Again', '/raphtalia', m)
 }
-handler.command = /^(hololive)$/i
+handler.command = /^(raphtalia)$/i
 handler.tags = ['fun']
-handler.help = ['hololive']
+handler.help = ['raphtalia']
 module.exports = handler
