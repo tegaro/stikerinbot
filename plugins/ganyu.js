eeca0689892ce8bd5e3d6a1c1bce5848a5eb3c9f
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-07 22:25:08 +0700

    Update ganyu.js

--- plugins/ganyu.js
+++ plugins/ganyu.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/ganyu.j
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Hololive', '© ollie', 'Get Again', '/ganyu', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Ganyu mpshhh', '© ollie', 'Get Again', '/ganyu', m)
 }
 handler.command = /^(ganyu)$/i
 handler.tags = ['anime']
