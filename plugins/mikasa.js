444da19162207daa073ca9d6098eb5b158863fdf
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-07 20:53:48 +0700

    Create mikasa.js

---
+++ plugins/mikasa.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/mikasa.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Mikasa Mpshhhh', '© ollie', 'Get Again', '/mikasa', m)
+}
+handler.command = /^(mikasa)$/i
+handler.tags = ['fun']
+handler.help = ['mikasa']
+module.exports = handler
