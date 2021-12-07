3c59c68d81a1c9fbd4fb1085adfb366f459a3f14
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-07 18:50:18 +0700

    Update pussy.js

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -2,7 +2,7 @@ let fetch = require('nekos.life')
 let { webp2mp4 } = require('../lib/webp2mp4')
 let nekos = new fetch()
 let handler = async (m, { conn, text, command }) => {
-if (!DATABASE.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
+if (!db.data.settings.nsfw) throw "Mode NSFW tidak aktif";
   m.reply(`_*Tunggu permintaan anda sedang diproses...*_`)
   let { url } = await nekos.nsfw.pussy()
   let buffer = await getBuffer(url)
@@ -10,7 +10,7 @@ if (!DATABASE.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\
   conn.sendMessage(m.chat, { url: result }, 'videoMessage', { mimetype: 'video/gif', quoted: m, caption: 'Huu sange sama kartun....' })
 }
 handler.command = /^(pussy)$/i
-handler.tags = ['sange']
+handler.tags = ['internet']
 handler.help = ['pussy']
 handler.limit = true
 
