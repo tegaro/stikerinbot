5594037eb288a2b25bf7cb8c230416b417857f22
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-09 23:42:11 +0700

    Create yuri.js

---
+++ plugins/yuri.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://hardianto.xyz/api/anime/random?nsfw=yuri&apikey=hardianto')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Yuri', '© ollie', 'Get Again', '/yuri', m)
+}
+handler.command = /^(yuri)$/i
+handler.tags = ['anime']
+handler.help = ['yuri']
+module.exports = handler
