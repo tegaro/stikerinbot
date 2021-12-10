e565bedc8fa8c8f8fda676a49ec7329f0588fa51
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 04:13:07 +0700

    Update ahegao.js

--- plugins/ahegao.js
+++ plugins/ahegao.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
     try {
-      if (!db.data.settings.nsfw) throw "Mode NSFW tidak aktif";
+      if (!db.data.settings.nsfw) throw "Admin grup belum mengaktifkan mode NSFW";
         let res = await fetch(global.API('xteam', '/randomimage/ahegao', {}, 'APIKEY'))
         if (res.status != 200) throw await res.text()
         let img = await res.buffer()
         conn.sendFile(m.chat, img, '', '*© ollie*', m, false, { thumbnail: Buffer.alloc(0) })
     } catch (e) {
-        throw `Admin belum mengaktifkan mode NSFW`
+        throw `Admin grup belum mengaktifkan mode NSFW`
     }
 }
 handler.help = ['ahegao']
