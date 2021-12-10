82d68529396d8dcc165d73eae4eb6c2cf29f902e
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 03:36:39 +0700

    Update wallpaperAnime.js

--- plugins/wallpaperAnime.js
+++ plugins/wallpaperAnime.js
@@ -13,6 +13,6 @@ let handler = async (m, { conn }) => {
 handler.help = ['wallpaperanime']
 handler.tags = ['anime']
 handler.command = /^(wallpaper|wp)anime$/i
-handler.limit = true
+handler.limit = false
 
 module.exports = handler
