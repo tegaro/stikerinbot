ae0f5f9b6b76290cbd3f437a5ca4ad9d70e0b858
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 16:40:18 +0700

    Create hololewd.js

---
+++ plugins/hololewd.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/hololewd.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random thighs', '© ollie', 'Get Again', '/hololewd', m)
+}
+handler.command = /^(hololewd)$/i
+handler.tags = ['anime']
+handler.help = ['hololewd']
+handler.nsfw = true
+module.exports = handler
