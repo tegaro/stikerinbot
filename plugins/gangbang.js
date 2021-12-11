3c43d397066435d9d240bceb6f505e924bb8daca
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 20:05:27 +0700

    Update gangbang.js

--- plugins/gangbang.js
+++ plugins/gangbang.js
@@ -1,7 +1,6 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-    if (!db.data.settings.nsfw) throw "Admin Grup belum mengaktifkan mode NSFW";
 let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/gangbang.json')
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
@@ -11,4 +10,6 @@ await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Holo
 handler.command = /^(gangbang)$/i
 handler.tags = ['anime']
 handler.help = ['gangbang']
+handler.nsfw = true
+
 module.exports = handler
