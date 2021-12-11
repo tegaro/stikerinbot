da4dde4a013e037a436f2348920e66a374173f88
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 19:55:44 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -81,7 +81,6 @@ module.exports = {
           if (!('sDemote' in chat)) chat.sDemote = ''
           if (!('descUpdate' in chat)) chat.descUpdate = true
           if (!('stiker' in chat)) chat.stiker = false
-          if (!('nsfw' in chat)) chat.nsfw = true
           if (!('delete' in chat)) chat.delete = true
           if (!('antiLink' in chat)) chat.antiLink = false
           if (!isNumber(chat.expired)) chat.expired = 0
@@ -90,7 +89,6 @@ module.exports = {
           if (!('viewonce' in chat)) chat.viewonce = true
         } else global.db.data.chats[m.chat] = {
           isBanned: false,
-          nsfw: false,
           welcome: false,
           detect: false,
           sWelcome: '',
@@ -120,6 +118,7 @@ module.exports = {
           if (!isNumber(settings.backupTime)) settings.backupTime = 0
           if (!'group' in settings) settings.group = false
           if (!'jadibot' in settings) settings.jadibot = false
+          if (!'nsfw' in settings) settings.nsfw = true
           if (!'restrict' in settings) settings.restrict = false
           if (!isNumber(settings.status)) settings.status = 0
         } else global.db.data.settings[this.user.jid] = {
@@ -133,6 +132,7 @@ module.exports = {
           backupTime: 0,
           group: false,
           jadibot: false,
+          nsfw: true,
           restrict: false,
           status: 0,
         }
@@ -275,6 +275,10 @@ module.exports = {
           }
           if (plugin.register == true && _user.registered == false) { // Butuh daftar?
             fail('unreg', m, this)
+            continue
+          }
+          if (plugin.nsfw && !global.db.data.settings.nsfw) { // Nsfw
+            fail('nsfw', m, this)
             continue
           }
 
@@ -496,7 +500,7 @@ global.dfail = (type, m, conn) => {
     private: 'Perintah ini hanya dapat digunakan di Chat Pribadi',
     admin: 'Perintah ini hanya untuk *Admin* grup',
     botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan perintah ini',
-    unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Ucup.19*',
+    unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Arif.19*',
     nsfw: 'NSFW tidak aktif'
   }[type]
   if (msg) return m.reply(msg)
