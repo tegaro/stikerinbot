5fff3446e2bed39c54254df8929ddd504a3923ae
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-12 03:04:06 +0700

    Create tits.js

---
+++ plugins/tits.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/tits.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random tits', '© ollie', 'Get Again', '/tits', m)
+}
+handler.command = /^(tits)$/i
+handler.tags = ['anime']
+handler.help = ['tits']
+module.exports = handler
