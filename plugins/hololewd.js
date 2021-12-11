c15be00289720a537ced5584a18604a9fca57794
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-12 01:50:59 +0700

    Update hololewd.js

--- plugins/hololewd.js
+++ plugins/hololewd.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/hololew
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random thighs', '© ollie', 'Get Again', '/hololewd', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random hololewd', '© ollie', 'Get Again', '/hololewd', m)
 }
 handler.command = /^(hololewd)$/i
 handler.tags = ['anime']
