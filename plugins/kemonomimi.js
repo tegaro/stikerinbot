6fab4633dbb56ab0a4aa805df466076cc4ca358b
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-12 03:02:30 +0700

    Create kemonomimi.js

---
+++ plugins/kemonomimi.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/kemonomimi.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random kemonomimi', '© ollie', 'Get Again', '/kemonomimi', m)
+}
+handler.command = /^(kemonomimi)$/i
+handler.tags = ['anime']
+handler.help = ['kemonomimi']
+module.exports = handler
