f49eda85832733d91c47beca0e662de82a6ad673
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 12:28:06 +0700

    Update thighs.js

--- plugins/thighs.js
+++ plugins/thighs.js
@@ -1,13 +1,18 @@
-let fetch = require('node-fetch')
+const fetch = require('node-fetch')
 
-let handler = async (m, { conn, text }) => {
-let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/thighs.json')
-if (!res.ok) throw await `${res.status} ${res.statusText}`;
-let json = await res.json();
-let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random thighs', '© ollie', 'Get Again', '/thighs', m)
+let handler = async (m, { conn }) => {
+    try {
+        let res = await fetch(global.API('mel', '/nsfw/thighs', {}, 'APIKEY'))
+        if (res.status != 200) throw await res.text()
+        let img = await res.buffer()
+        conn.sendFile(m.chat, img, '', '*© ollie*', m, false, { thumbnail: Buffer.alloc(0) })
+    } catch (e) {
+        throw `Limit apikey habis atau error!`
+    }
 }
-handler.command = /^(thighs)$/i
-handler.tags = ['anime']
 handler.help = ['thighs']
+handler.tags = ['anime']
+handler.command = /^(thighs)$/i
+handler.limit = false
+
 module.exports = handler
