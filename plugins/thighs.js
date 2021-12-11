62b62d481b04d2eb4eda3a050764acdcc2820ce7
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 16:30:03 +0700

    Create thighs.js

---
+++ plugins/thighs.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/thighs.json')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random armpits', '© ollie', 'Get Again', '/thighs', m)
+}
+handler.command = /^(thighs)$/i
+handler.tags = ['anime']
+handler.help = ['thighs']
+handler.nsfw = true
+module.exports = handler
