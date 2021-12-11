126f85b1d02da992c4ea9e53b8798fb813190ed0
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 12:44:40 +0700

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,14 +1,25 @@
+// Pngocok handal
+
 let fetch = require('node-fetch')
-let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} dandelions`
-  let res = await fetch(global.API('bx', '/api/music/liriklagu', { query: text }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
+let handler = async (m, { text }) => {
+  let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
+    title: text
+  }))
+  if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.status) throw json
-  m.reply(json.result)
+  if (!json.thumbnail.genius) throw json
+  conn.sendFile(m.chat, json.thumbnail.genius, '', `
+*${json.title}*
+_${json.author}_
+
+${json.lyrics}
+
+
+${json.links.genius}
+`, m)
 }
-handler.help = ['lirik'].map(v => v + ' <teks>')
+handler.help = ['lirik'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
-handler.command = /^(lirik|lyrics?)$/i
+handler.command = /^(lirik|lyrics|lyric)$/i
 
 module.exports = handler
