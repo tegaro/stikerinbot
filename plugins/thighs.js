797cf2c6f55b924c66f8cb21157f0ec2762ecf66
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 16:30:22 +0700

    Update thighs.js

--- plugins/thighs.js
+++ plugins/thighs.js
@@ -5,7 +5,7 @@ let res = await fetch('https://raw.githubusercontent.com/tegaro/uwu/main/thighs.
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random armpits', '© ollie', 'Get Again', '/thighs', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random thighs', '© ollie', 'Get Again', '/thighs', m)
 }
 handler.command = /^(thighs)$/i
 handler.tags = ['anime']
