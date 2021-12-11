62b3a5775cf4abef1106e5ea68e41a29f52a81b0
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-11 19:41:10 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -277,7 +277,7 @@ module.exports = {
             fail('unreg', m, this)
             continue
           }
-          if (plugin.nsfw && !isAdmin.db.data.settings.nsfw) { // Nsfw
+          if (plugin.nsfw && !isAdmin) { // Nsfw
             fail('nsfw', m, this)
             continue
           }
