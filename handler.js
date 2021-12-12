f9fa3754cb707f14ed60798bfef4da71837fb08a
tegaro <92983699+tegaro@users.noreply.github.com>
2021-12-12 11:37:38 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -87,6 +87,7 @@ module.exports = {
           if (!('antiBadword' in chat)) chat.antiBadword = true
           if (!('getmsg' in chat)) chat.getmsg = false
           if (!('viewonce' in chat)) chat.viewonce = true
+          if (!('nsfw' in chat)) chat.nsfw = true
         } else global.db.data.chats[m.chat] = {
           isBanned: false,
           welcome: false,
@@ -103,6 +104,7 @@ module.exports = {
           antiBadword: true,
           getmsg: false,
           viewonce: true,
+          nsfw: true,
         }
 
         let settings = global.db.data.settings[this.user.jid]
@@ -118,7 +120,6 @@ module.exports = {
           if (!isNumber(settings.backupTime)) settings.backupTime = 0
           if (!'group' in settings) settings.group = false
           if (!'jadibot' in settings) settings.jadibot = false
-          if (!'nsfw' in settings) settings.nsfw = true
           if (!'restrict' in settings) settings.restrict = false
           if (!isNumber(settings.status)) settings.status = 0
         } else global.db.data.settings[this.user.jid] = {
@@ -132,7 +133,6 @@ module.exports = {
           backupTime: 0,
           group: false,
           jadibot: false,
-          nsfw: true,
           restrict: false,
           status: 0,
         }
